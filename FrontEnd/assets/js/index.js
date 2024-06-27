e5fc061b643a53362f030675aa90abe6cbacc4e5
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2024-06-27 12:09:18 +0200

    création bouton modifier + barre mode édition

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// ********** CONSTANTS *********** //
+
+const gallery = document.querySelector(".gallery");
+
+
+
+
+// ********** VARIABLES *********** //
+
+
+
+
+
+
+
+// ********** FUNCTIONS *********** //
+
+
+
+
+
+
+
+// ********** MAIN *********** //
+
+
+
+
---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+// ********** CONSTANTS *********** //
+
+const email = document.querySelector('#email');
+
+const submit = document.querySelector('[type="submit"]');
+
+const password = document.querySelector('#password');
+
+const error = document.querySelector('#error')
+
+console.log("password")
+
+
+// ********** VARIABLES *********** //
+
+
+
+
+
+
+
+// ********** FUNCTIONS *********** //
+
+
+
+
+
+
+
+// ********** MAIN *********** //
+
+
+
+
+
